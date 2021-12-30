2645931cac403c7b3c1d6351e449db476f59070c
hund <utiful2004@gmail.com>
2021-12-30 17:19:48 +0900

    test

--- src/main/java/com/mindeulle/yoga/calendar/boot/CalendarApplication.java
+++ src/main/java/com/mindeulle/yoga/calendar/boot/CalendarApplication.java
@@ -1,15 +1,19 @@
 package com.mindeulle.yoga.calendar.boot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.mindeulle.yoga.calendar"})
 public class CalendarApplication {
 
     public static void main(String[] args) {
+        //
         SpringApplication.run(CalendarApplication.class, args);
+        log.info("Boot Started");
     }
 
 }
