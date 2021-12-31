62b52e03a0e3403e7d10859e493da49573cfeedc
hund <utiful2004@gmail.com>
2021-12-31 09:38:36 +0900

    heroku test 3

--- src/main/java/com/mindeulle/yoga/calendar/rest/CalendarRestController.java
+++ src/main/java/com/mindeulle/yoga/calendar/rest/CalendarRestController.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/mindeulle/yoga/calendar")
 public class CalendarRestController {
@@ -16,11 +19,16 @@ public class CalendarRestController {
     }
 
     @CrossOrigin
-    @PostMapping  ("/test")
+    @PostMapping("/test")
     public String execute(@RequestBody String something) {
         //
         logger.info(something);
         return "hello";
     }
 
+    @GetMapping("/test2")
+    public void execute(HttpServletResponse response) throws IOException {
+        String redirect_uri="http://www.google.com";
+        response.sendRedirect(redirect_uri);
+    }
 }
