b29db2b7f52883bcc4bdc315547513b7dc70ae82
Andrew Lee <utiful2004@gmail.com>
2022-01-01 17:57:57 +0900

    credential changes

--- src/main/java/com/mindeulle/yoga/calendar/schedule/MessageScheduler.java
+++ src/main/java/com/mindeulle/yoga/calendar/schedule/MessageScheduler.java
@@ -56,7 +56,12 @@ public class MessageScheduler {
 
     private final WebClient webClient;
 
-    private static final String APPLICATION_NAME = "My Project 99409";
+    private static final String APPLICATION_NAME = "mindeulle-calendar";
+    private static final String GOOGLE_CALENDAR_ID = "1tq87lipcdr56pfavrevq517r8@group.calendar.google.com";
+    private static final String NC_SERVICE_ID = "ncp:sms:kr:277938056372:yoga-sms";
+    private static final String NC_ACCESS_KEY = "a5imXcry3TDgoS3vD6rD";
+    private static final String NC_SECRET_KEY = "h7AP1GsKucfIkOFQao3BjyotCJWyEMdXpxj9bDET";
+    private static final String OWNER_MOBILE = "01020252785";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
     private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_EVENTS_READONLY);
@@ -94,7 +99,7 @@ public class MessageScheduler {
         DateTime nextDayStartDateTime = new DateTime(nextDayStartTimeMillis);
         DateTime nextDayEndDateTime = new DateTime(nextDayEndTimeMillis);
 
-        Events events = service.events().list("vlvethund@gmail.com")
+        Events events = service.events().list(GOOGLE_CALENDAR_ID)
                 .setTimeMin(nextDayStartDateTime)
                 .setTimeMax(nextDayEndDateTime)
                 .setOrderBy("startTime")
@@ -120,10 +125,9 @@ public class MessageScheduler {
 
         MessageRequest messageRequest = new MessageRequest();
         StringBuilder ownerMessage = new StringBuilder("내일 예약 현황입니다 \n");
-        String ownerMobile = "01046706532";
 
         messageRequest.setType("MMS");
-        messageRequest.setFrom(ownerMobile);
+        messageRequest.setFrom(OWNER_MOBILE);
         messageRequest.setSubject("수업 예약 알림");
         messageRequest.setContent("안녕하세요, 민들레요가 입니다." +
                 " 내일 수업이 예약되어있습니다." +
@@ -140,7 +144,7 @@ public class MessageScheduler {
                 Message message = new Message();
                 message.setTo(mobile);
                 message.setContent(String.format("안녕하세요 %s님, 민들레요가입니다." +
-                        " 내일 [%s] 타임 수업이 예약되어있습니다." +
+                        " 내일 %s 타임 수업이 예약되어있습니다." +
                         " 당일 취소는 횟수 차감이니" +
                         " 전날 취소해주세요", name, key));
                 messageRequest.getMessages().add(message);
@@ -151,15 +155,12 @@ public class MessageScheduler {
 
         Message toOwner = new Message();
         toOwner.setSubject("수업 예약 현황");
-        toOwner.setTo(ownerMobile);
+        toOwner.setTo(OWNER_MOBILE);
         toOwner.setContent(ownerMessage.toString());
         messageRequest.getMessages().add(toOwner);
 
-        String serviceId = "ncp:sms:kr:277710741014:mindeulle";
-        String accessKey = "y3SlFARFn8SMZb8PVy2o";
-        String secretKey = "pFx8dMCekdKu0mA2uLA6zLRw9JCDP2NkhZhksFdy";
         long currentTime = System.currentTimeMillis();
-        String signature = makeSignature(serviceId, accessKey, secretKey, currentTime);
+        String signature = makeSignature(NC_SERVICE_ID, NC_ACCESS_KEY, NC_SECRET_KEY, currentTime);
 
         if (messageRequest.getMessages().size() == 0) {
             log.info("No Messages To Send. Current Message Size: " + messageRequest.getMessages().size());
@@ -167,12 +168,12 @@ public class MessageScheduler {
         }
 
         Mono<MessageRespond> messageRespondMono = webClient.post()
-                .uri(String.format("/v2/services/%s/messages", serviceId))
+                .uri(String.format("/v2/services/%s/messages", NC_SERVICE_ID))
                 .headers(headers -> {
                     headers.add("Content-Type", "application/json; charset=utf-8");
                     headers.add("x-ncp-apigw-signature-v2", signature);
                     headers.add("x-ncp-apigw-timestamp", String.valueOf(currentTime));
-                    headers.add("x-ncp-iam-access-key", accessKey);
+                    headers.add("x-ncp-iam-access-key", NC_ACCESS_KEY);
                 })
                 .body(Mono.just(messageRequest), MessageRequest.class)
                 .retrieve()
